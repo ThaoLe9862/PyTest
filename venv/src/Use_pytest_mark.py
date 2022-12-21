de9f45f88bf89ed62803840b82021e096f498c41
tgnguyen <tgnguyen@temps-Air.attlocal.net>
2022-12-21 12:59:47 -0800

    Add @pytest.mark.xxx and how to run multi-test cases

--- venv/src/Use_pytest_mark.py
+++ venv/src/Use_pytest_mark.py
@@ -1,6 +1,9 @@
 '''
-1. To run test with command: pytest FirstTest.py
-2. To run test with select test name = minus: pytest FirstTest.py -k minus -v
+1. Add @pytest.mark.xxx before each test cases
+2. To run test cases depending on mark name:
+    pytest Use_pytest_mark.py -m more -v
+    or
+    pytest Use_pytest_mark.py -m less -v
 '''
 import pytest
 
@@ -18,17 +21,22 @@ def Divide(a, b):
     return a / b
 
 # Test cases (started with "test")
+@pytest.mark.more
 def test_add():
     assert Add(2, 3) == 5
 
+@pytest.mark.more
 def test_add_false():
     assert Add(2, 3) == 6
 
+@pytest.mark.less
 def test_minus():
     assert Minus(5, 3) == 2
 
+@pytest.mark.more
 def test_multiply():
     assert Multiply(2, 3) == 6
 
+@pytest.mark.five
 def test_divide():
     assert Divide(6, 3) == 2
